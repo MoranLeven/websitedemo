8d48999ab12ada829f92e4ca6d9aaf8d3ebcaed4
MoranLeven <49155163+MoranLeven@users.noreply.github.com>
2021-03-18 17:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Hotelier", "Dancer", "choreographer"],
+        strings: ["Hotelier", "Dancer", "choreographer", "Professional-Photographer"],
         typeSpeed: 80,
         backSpeed: 70,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Hotelier", "Dancer", "choreographer"],
+        strings: ["Hotelier", "Dancer", "choreographer", "Professional-Photographer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
